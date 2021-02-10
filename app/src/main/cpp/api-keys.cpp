b3cada2e9eee8a595096e164ff5398047d96748d
turkergoksu <turkergoksu@gmail.com>
2021-02-10 22:09:08 +0300

    Dummy commit

--- app/src/main/cpp/api-keys.cpp
+++ app/src/main/cpp/api-keys.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring
 
 JNICALL
 Java_me_turkergoksu_kefilm_ApiKeyLibrary_getMovieDbApiKeyFromJNI(JNIEnv *env, jobject object) {
-    std::string movieDbApiKey = "YOUR_API_KEY_GOES_HERE";
+    std::string movieDbApiKey = "TEST";
     return env->NewStringUTF(movieDbApiKey.c_str());
 }
