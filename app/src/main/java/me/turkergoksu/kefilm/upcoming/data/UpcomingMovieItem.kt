d696b46ea53638b82c95b02d4e6f4b7cdf15e1e2
turkergoksu <turkergoksu@gmail.com>
2021-06-09 21:53:13 +0300

    Add UpcomingMovieItem.kt

---
+++ app/src/main/java/me/turkergoksu/kefilm/upcoming/data/UpcomingMovieItem.kt
@@ -0,0 +1,12 @@
+package me.turkergoksu.kefilm.upcoming.data
+
+/**
+ * Created by turkergoksu on 09-Jun-21.
+ */
+data class UpcomingMovieItem(
+    val id: Int,
+    val posterPath: String,
+    val title: String,
+    val releaseDate: String,
+    val overview: String
+)
