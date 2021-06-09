6202b7723b2db6950699480d311de460c41fa212
turkergoksu <turkergoksu@gmail.com>
2021-06-09 22:55:11 +0300

    Add Medusa

--- app/src/main/java/me/turkergoksu/kefilm/MainActivity.kt
+++ app/src/main/java/me/turkergoksu/kefilm/MainActivity.kt
@@ -2,13 +2,61 @@ package me.turkergoksu.kefilm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.trendyol.medusalib.navigator.MultipleStackNavigator
+import com.trendyol.medusalib.navigator.Navigator
 import me.turkergoksu.kefilm.databinding.ActivityMainBinding
+import me.turkergoksu.kefilm.upcoming.ui.UpcomingFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Navigator.NavigatorListener {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val rootFragmentProvider: List<() -> Fragment> =
+        listOf(
+            { UpcomingFragment.newInstance() },
+        )
+
+    val navigator: MultipleStackNavigator =
+        MultipleStackNavigator(
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.fragmentContainer,
+            rootFragmentProvider = rootFragmentProvider,
+            navigatorListener = this
+        )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navigator.initialize(savedInstanceState)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.upcoming -> {
+                    navigator.switchTab(0)
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
+
+    override fun onTabChanged(tabIndex: Int) {
+        when (tabIndex) {
+            0 -> binding.bottomNavigationView.selectedItemId = R.id.upcoming
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        navigator.onSaveInstanceState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onBackPressed() {
+        if (navigator.canGoBack()) navigator.goBack()
+        else super.onBackPressed()
     }
 }
--- app/src/main/java/me/turkergoksu/kefilm/upcoming/ui/UpcomingFragment.kt
+++ app/src/main/java/me/turkergoksu/kefilm/upcoming/ui/UpcomingFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import me.turkergoksu.kefilm.core.BaseFragment
 import me.turkergoksu.kefilm.databinding.FragmentUpcomingBinding
+import me.turkergoksu.kefilm.upcoming.data.UpcomingMovieItem
 
 /**
  * Created by turkergoksu on 09-Jun-21.
@@ -18,5 +19,33 @@ class UpcomingFragment : BaseFragment<FragmentUpcomingBinding, UpcomingViewModel
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val adapter = UpcomingMovieItemAdapter()
+        binding.recyclerViewUpcomingList.adapter = adapter
+
+        val dummyList = listOf(
+            UpcomingMovieItem(
+                id = 0,
+                posterPath = "",
+                title = "myTitle",
+                releaseDate = "date",
+                overview = "overviiiew"
+            ),
+            UpcomingMovieItem(
+                id = 1,
+                posterPath = "",
+                title = "myTitleeee",
+                releaseDate = "dateee",
+                overview = "overviiiewee"
+            )
+        )
+        adapter.submitList(dummyList)
+    }
+
+    companion object {
+        fun newInstance() = UpcomingFragment()
     }
 }
