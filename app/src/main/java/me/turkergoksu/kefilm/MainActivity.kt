1dd6f2ad0deff50621698cb408d017bf3b6ef740
turkergoksu <turkergoksu@gmail.com>
2021-06-09 20:56:32 +0300

    Convert MainActivity.kt to data binding

--- app/src/main/java/me/turkergoksu/kefilm/MainActivity.kt
+++ app/src/main/java/me/turkergoksu/kefilm/MainActivity.kt
@@ -2,11 +2,13 @@ package me.turkergoksu.kefilm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import me.turkergoksu.kefilm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
