a2f10ac9509e07de90020e178eba8e4fd225b78c
turkergoksu <turkergoksu@gmail.com>
2021-06-11 00:40:51 +0300

    Add KefilmApplication.kt

---
+++ app/src/main/java/me/turkergoksu/kefilm/KefilmApplication.kt
@@ -0,0 +1,10 @@
+package me.turkergoksu.kefilm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by turkergoksu on 11-Jun-21.
+ */
+@HiltAndroidApp
+class KefilmApplication : Application()
