df1757bd21b73893bf439e44488aea75969053fd
turkergoksu <turkergoksu@gmail.com>
2021-06-12 00:47:10 +0300

    Add upcoming request to MovieService.kt

--- app/src/main/java/me/turkergoksu/kefilm/api/MovieService.kt
+++ app/src/main/java/me/turkergoksu/kefilm/api/MovieService.kt
@@ -1,8 +1,14 @@
 package me.turkergoksu.kefilm.api
 
+import me.turkergoksu.kefilm.upcoming.data.remote.UpcomingResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
 /**
  * Created by turkergoksu on 11-Jun-21.
  */
 interface MovieService {
 
+    @GET("/3/movie/upcoming")
+    suspend fun getUpcomingMovies(): Response<UpcomingResponse>
 }
