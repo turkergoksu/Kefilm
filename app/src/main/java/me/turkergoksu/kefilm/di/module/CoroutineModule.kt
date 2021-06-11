7b6163de284b24134a89f875885ba0459e555771
turkergoksu <turkergoksu@gmail.com>
2021-06-11 23:10:31 +0300

    Add CoroutineModule.kt

---
+++ app/src/main/java/me/turkergoksu/kefilm/di/module/CoroutineModule.kt
@@ -0,0 +1,41 @@
+package me.turkergoksu.kefilm.di.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+/**
+ * Created by turkergoksu on 11-Jun-21.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class CoroutineModule {
+
+    @DefaultDispatcher
+    @Provides
+    internal fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    internal fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    internal fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
