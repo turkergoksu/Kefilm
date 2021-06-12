5c3a021bc41b7730364225f9bb35b6009716cbba
turkergoksu <turkergoksu@gmail.com>
2021-06-12 23:22:42 +0300

    Change movie detail design

--- app/src/main/java/me/turkergoksu/kefilm/movie_detail/ui/MovieDetailViewState.kt
+++ app/src/main/java/me/turkergoksu/kefilm/movie_detail/ui/MovieDetailViewState.kt
@@ -1,5 +1,19 @@
 package me.turkergoksu.kefilm.movie_detail.ui
 
+import me.turkergoksu.kefilm.Constants
 import me.turkergoksu.kefilm.movie_detail.data.MovieDetail
 
-data class MovieDetailViewState(val movieDetail: MovieDetail)
+data class MovieDetailViewState(val movieDetail: MovieDetail) {
+
+    fun getRating() = movieDetail.voteAverage.toFloat() / 2f
+
+    fun getVoteCountText() = String.format("(%s)", movieDetail.voteCount)
+
+    fun getTitle() = movieDetail.title
+
+    fun getYear() = movieDetail.releaseYear
+
+    fun getPosterUrl() = Constants.API_IMAGE_URL + movieDetail.posterPath
+
+    fun getOverview() = movieDetail.overview
+}
