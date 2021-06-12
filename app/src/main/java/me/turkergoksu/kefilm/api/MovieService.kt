2f190fe35e0e9555ab4b31386153db1ca275c448
turkergoksu <turkergoksu@gmail.com>
2021-06-12 21:42:14 +0300

    Add movie detail request in MovieService.kt

--- app/src/main/java/me/turkergoksu/kefilm/api/MovieService.kt
+++ app/src/main/java/me/turkergoksu/kefilm/api/MovieService.kt
@@ -1,8 +1,10 @@
 package me.turkergoksu.kefilm.api
 
+import me.turkergoksu.kefilm.movie_detail.data.remote.MovieDetailResponse
 import me.turkergoksu.kefilm.upcoming.data.remote.UpcomingResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 /**
  * Created by turkergoksu on 11-Jun-21.
@@ -11,4 +13,7 @@ interface MovieService {
 
     @GET("/3/movie/upcoming")
     suspend fun getUpcomingMovies(): Response<UpcomingResponse>
+
+    @GET("/3/movie/{movie_id}")
+    suspend fun getMovieDetail(@Path("movie_id") movieId: Int): Response<MovieDetailResponse>
 }
